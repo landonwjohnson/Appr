f778065048726935b485f038a46b9b10972d5bf2
Landon <landonwjohnson@gmail.com>
2017-11-17 13:01:40 -0700

    laying out the front end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './css/styles.css';
 import Header from './components/Header';
 import Home from './components/content/Home';
+import Register from './components/Register';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Home />
+          <Home/>
       </div>
     );
   }
