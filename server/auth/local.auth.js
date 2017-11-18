0e1355dc181ca0cf209d67803e73a63c4451613b
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-18 11:43:39 -0700

    serialize user

--- server/auth/local.auth.js
+++ server/auth/local.auth.js
@@ -1,2 +1,10 @@
 const passport = require('passport');
 const Strategy = require('passport-local'.Strategy);
+//const getDb = require('../database/bootstrap.database');
+
+passport.use( /* Login Auth Strategy */ );
+passport.use( /* Logout Auth Strategy */ );
+
+passport.serializeUser(( user, done ) => {
+    return done(null, user.id);
+});
