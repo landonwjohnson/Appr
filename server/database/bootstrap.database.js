727117d048da9de98aa939b01efdd1da34ce38f4
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-18 12:00:23 -0700

    massive is used

--- server/database/bootstrap.database.js
+++ server/database/bootstrap.database.js
@@ -2,3 +2,7 @@ const massive = require('massive');
 //const connectionString = require('./connection.database.js');
 
 let db;
+
+massive(connectionString)
+    .then( dbInstance => db = dbInstance )
+    .catch( err => {throw err});
