9b23a907417eca937a0830cb62b5ee9301b600b0
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-18 11:37:30 -0700

    wrote code for decorate(app)

--- server/middleware/decorate.middleware.js
+++ server/middleware/decorate.middleware.js
@@ -6,7 +6,19 @@ const helmet = require('helmet');
 //const passport = require('../auth/local.auth');
 
 function decorate(app) {
-    //code here
+    //app.use(express.static(/*directory of build folder*/));
+    app.use(bodyParser.JSON());
+    app.use(cors());
+    app.use(helmet());
+    app.use(session({
+        secret: 'It belongs in a museum!',
+        resave: false,
+        saveUninitialized: true
+    }));
+    //app.use(passport.initialize());
+    //app.use(passport.session());
+
+    return app;
 }
 
 module.exports = decorate;
