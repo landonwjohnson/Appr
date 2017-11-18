cd293b1f01189d8e33dbc612993fbf000e5384bb
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-18 11:28:24 -0700

    installed helmet and imported into decorate.middleware.js

--- server/middleware/decorate.middleware.js
+++ server/middleware/decorate.middleware.js
@@ -2,3 +2,4 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
