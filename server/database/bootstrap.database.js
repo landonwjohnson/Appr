ba1717f28cba6605633ed269e3537dcb7a079edf
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-18 12:01:18 -0700

    define getDb function

--- server/database/bootstrap.database.js
+++ server/database/bootstrap.database.js
@@ -6,3 +6,10 @@ let db;
 massive(connectionString)
     .then( dbInstance => db = dbInstance )
     .catch( err => {throw err});
+
+function getDb() {
+    if (!db) {
+        console.error('We haven\'t connected to the database yet!');
+    }
+    return db;
+}
