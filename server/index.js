4b66d0d1243012573593337cb62b3d5e6e470b21
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-18 11:19:42 -0700

    Installed nodemon, set up server to listen.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
+const chalk = require('chalk');
 const express = require('express');
+
 const app = express();
 
 const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(chalk.green(`Server is listening on port ${chalk.cyan(port)}.`));
+});
