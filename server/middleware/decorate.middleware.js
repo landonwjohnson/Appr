14d5f5dd454e245ebb31ad6a77963b6ab044e3fb
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-18 11:30:47 -0700

    defined function decorate

--- server/middleware/decorate.middleware.js
+++ server/middleware/decorate.middleware.js
@@ -3,3 +3,10 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+//const passport = require('../auth/local.auth');
+
+function decorate(app) {
+    //code here
+}
+
+module.exports = decorate;
