3faaaf2ea9d4df125a362cc29c4c03672a5b0fd9
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-18 12:53:09 -0700

    imported/use delegate routes in server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const chalk = require('chalk');
 const express = require('express');
 const addMiddlewareTo = require('./middleware/decorate.middleware');
+const delegateRoutesFor = require('./routers/delegate.routers');
 
 const app = express();
 
 addMiddlewareTo(app);
+delegateRoutesFor(app);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
