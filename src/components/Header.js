f45474f5e7938b02ad3e6688fd5da288043c7d87
landonwjohnson <landonwjohnson@gmail.com>
2017-11-18 16:16:04 -0700

    styled the left menu for the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,21 @@ import UserAvatar from '../img/placeholders/Landon-Thumb-Grey.jpg';
 import AlertIcon from '../img/icons/Bell-02.svg';
 import BoardsIcon from '../img/icons/boards.svg';
 
+
+
 class Header extends Component {
-  render() {
+
+    
+    
+  render(){
+    let NavX = function() {
+        document.getElementById("bread-menu-toggle").classList.toggle("active");
+        }
+  
+  
     return (
       <div>
-
+          <div className="board-menu-container"></div>
           <div className="header-container">
                 <div className="nav-bar">
                     <div className="board-con">
@@ -19,18 +29,42 @@ class Header extends Component {
                         <div className="logo">Appr</div>
                     </div>
                 <div className="user-con">
-                    <div className="avatar"><img src={UserAvatar} /></div>
-                    <div className="hello-user">Hello Jaysen!</div>
-                    <div className="alert-icon"><img src={AlertIcon} /></div>
-                    <div className="menu-btn">
-                    <div className="menu-icon">
-                        <div className="line"> </div>
-                        <div className="line"> </div>
-                    </div>
-                    </div>
+                 
+                        <div className="avatar"><img src={UserAvatar} /></div>
+                        <div className="hello-user">Hello Jaysen!</div>
+                        <div className="alert-icon"><img src={AlertIcon} /></div>
+                        <div id="bread-menu-toggle" href="#" onClick={NavX}>
+                            <div className="bread-menu">
+                                <div className="bread-top"> <span/> </div>
+                                <div className="bread-bottom"> <span/> </div>
+                            </div>
+                        </div> 
                 </div>
-                </div>
+               
+                </div>    
         </div>
+        <div className="left-menu-container">
+            <div className="left-menu-outter">
+            <div className="left-menu-inner">
+                <ul>
+                    <li>Profile</li>
+                    <li>Cards</li>
+                    <li>Settings</li>
+                    <li>Help</li>
+                    <li>Shortcuts</li>
+                    <li>Change Language</li>
+                    <li>Log Out</li>
+                
+                </ul>
+            </div>
+            </div>
+        </div>
+        
+      
+        
+        
+
+        
       </div>
     );
   }
