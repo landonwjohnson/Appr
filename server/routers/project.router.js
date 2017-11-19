a3ffdac983d391dc07e0a9a1d65b9146aaa11c8d
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-19 08:37:38 -0700

    created read project route

--- server/routers/project.router.js
+++ server/routers/project.router.js
@@ -7,6 +7,13 @@ projectRouter.post('/create', (req, res) => {
     const db = getDb();
     const { authorId } = req.body;
     db.create_project([ authorId ])
-        .then( () => res.status(200).send())
+        .then( promise => res.status(200).send( promise ))
+        .catch( err => res.send( err ));
+});
+
+projectRouter.get('/:projectid', (req, res) => {
+    const db = getDb();
+    db.find_project_by_id([ this.props.match.params.id ])
+        .then( project => res.status(200).send( project ))
         .catch( err => res.send( err ));
 });
