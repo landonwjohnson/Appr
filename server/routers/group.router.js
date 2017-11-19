ac48237d7342975ce3266b47e6e5881f7c5120e4
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-19 09:38:09 -0700

    legibility changes

--- server/routers/group.router.js
+++ server/routers/group.router.js
@@ -4,32 +4,35 @@ const getDb = require('../database/bootstrap.database');
 const groupRouter = express.Router();
 
 groupRouter.post('/create', (req, res) => {
-    const db = getDb();
     const { name, createdByUserId } = req.body;
-    db.create_group([name, createdByUserId])
-        .then( () => res.status(200).send())
+    const db = getDb();
+    db.create_group([ name, createdByUserId ])
+        .then( promise => res.send(promise))
         .catch( err => res.send(err));
 });
 
 groupRouter.get('/:groupid', (req, res) => {
+    const groupId = this.props.match.params.groupid;
     const db = getDb();
-    db.find_group_by_id([this.props.match.params.id])
+    db.find_group_by_id([ groupId ])
         .then( group => res.send(group))
         .catch( err => res.send(err));
 });
 
 groupRouter.put('/update/:groupid', (req, res) => {
-    const db = getDb();
+    const groupId = this.props.match.params.groupid;
     const { name } = req.body;
-    db.update_group([name])
-        .then( () => res.status(200).send())
+    const db = getDb();
+    db.update_group([ groupId, name ])
+        .then( promise => res.send(promise))
         .catch( err => res.send(err));
 });
 
 groupRouter.delete('/delete/:groupid', (req, res) => {
+    const groupId = this.props.match.params.groupid;
     const db = getDb();
-    db.delete_group([this.props.match.params.id])
-        .then( () => res.status(200).send())
+    db.delete_group([ groupId ])
+        .then( promise => res.send(promise))
         .catch( err => res.send(err));
 });
 
