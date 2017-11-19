b7227aa4c3077b94fc15819788a3acedfa6016f0
landonwjohnson <landonwjohnson@gmail.com>
2017-11-19 12:50:42 -0700

    fixed styling in Settings, Header, Login, and LandingSlider components

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,35 +10,86 @@ class Header extends Component {
     constructor(){
         super();
         this.state ={
-            leftMenuOpen: true,
+            rightMenuOpen: true,
+            boardMenuOpen: true,
         }
-        this.handleLeftMenuClick = this.handleLeftMenuClick.bind(this); 
+        this.handleRightMenuClick = this.handleRightMenuClick.bind(this); 
+        this.handleBoardMenuClick = this.handleBoardMenuClick.bind(this);
     }
 
-    handleLeftMenuClick(){
-        if(this.state.leftMenuOpen){
-            this.setState({leftMenuOpen: false})
+    handleRightMenuClick(){
+        if(this.state.rightMenuOpen){
+            this.setState({rightMenuOpen: false})
         }
         else {
-            this.setState({leftMenuOpen: true})
+            this.setState({rightMenuOpen: true})
         }
     }
 
+    handleBoardMenuClick(){
+        if(this.state.boardMenuOpen){
+            this.setState({boardMenuOpen: false})
+        }
+        else {
+            this.setState({boardMenuOpen: true})
+        }
+    }
     
     
   render(){
-    var leftMenuClass = classnames({
-        "left-menu-container--hide": this.state.leftMenuOpen,
-        "left-menu-container": true
+    let rightMenuClass = classnames({
+        "right-menu-container--hide": this.state.rightMenuOpen,
+        "right-menu-container": true
+    })
+
+    var boardMenuClass = classnames({
+        "board-menu-container--hide": this.state.boardMenuOpen,
+        "board-menu-container": true
     })
   
   
     return (
       <div>
-          <div className="board-menu-container"></div>
+          <div className={boardMenuClass}>
+          <a onClick={this.handleBoardMenuClick} href="#">close</a>
+            <div className="boards-main-container">
+            
+                <div className="recent-boards-con">
+                    <div className="text-12">RECENT BOARDS</div>
+                        <div className="board-menu-item">
+                            <div className="board-item-thumbnail">
+
+                            </div>
+                            <div className="board-item-name">
+                                Placeholder
+                            </div>
+                        </div>
+                    <div className="board-menu-item">
+                        <div className="board-item-thumbnail">
+
+                        </div>
+                        <div className="board-item-name">
+                            Placeholder
+                        </div>
+                    </div>
+               
+                </div>
+                <div className="personal-boards-con">
+                    <div className="text-12">PERSONAL BOARDS</div>
+                        <div className="create-board-item">
+                            <div className="create-board-thumbnail">
+                                <div className="plus-symbol">+</div>
+                            </div>
+                            <div className="create-board-name">
+                                Create Board
+                            </div>
+                        </div>
+                </div>
+            </div>
+          </div>
           <div className="header-container">
                 <div className="nav-bar">
-                    <div className="board-con">
+                    <div className="board-con" href="#" onClick={this.handleBoardMenuClick} >
                     <div className="board-icon"><img src={BoardsIcon} /></div>
                     
                     <div className="board-text">Boards</div>
@@ -49,9 +100,9 @@ class Header extends Component {
                 <div className="user-con">
                  
                         <div className="avatar"><img src={UserAvatar} /></div>
-                        <div className="hello-user">Hello Jaysen!</div>
+                        <div className="hello-user">Hello Landon!</div>
                         <div className="alert-icon"><img src={AlertIcon} /></div>
-                        <div id="bread-menu-toggle" href="#" onClick={this.handleLeftMenuClick} >
+                        <div id="bread-menu-toggle" href="#" onClick={this.handleRightMenuClick} >
                             <div className="bread-menu">
                                 <div className="bread-top"> <span/> </div>
                                 <div className="bread-bottom"> <span/> </div>
@@ -61,18 +112,17 @@ class Header extends Component {
                
                 </div>    
         </div>
-        <div className={leftMenuClass}>
-            <div className="left-menu-outter">
-            <div className="left-menu-inner">
+        <div className={rightMenuClass}>
+            <div className="right-menu-outter">
+            <div className="right-menu-inner">
                 <ul>
                     <li>Profile</li>
                     <li>Cards</li>
                     <li>Settings</li>
                     <li>Help</li>
                     <li>Shortcuts</li>
-                    <li>Change Language</li>
+                    <li>Report Bugs</li>
                     <li>Log Out</li>
-                
                 </ul>
             </div>
             </div>
