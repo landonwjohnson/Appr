b515a83dacd65e87e26cb12160ed4526f1eb8671
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-19 09:20:53 -0700

    legibility changes

--- server/routers/dashboard.router.js
+++ server/routers/dashboard.router.js
@@ -4,9 +4,10 @@ const getDb = require('../database/bootstrap.database');
 const dashboardRouter = express.Router();
 
 dashboardRouter.get('/:userid', (req, res) => {
+    const userId = this.props.match.params.userid;
     const db = getDb();
-    db.find_usergroups_and_userprojects([this.props.match.params.id])
-        .then( () => res.send())
+    db.find_usergroups_and_userprojects([ userId ])
+        .then( promise => res.send(promise))
         .catch( err => res.send(err));
 });
 
