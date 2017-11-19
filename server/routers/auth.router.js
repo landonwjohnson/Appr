ca8e627f926461a784706bcb697e400593a271f9
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-19 09:18:10 -0700

    added catch to find_user_by_email request

--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -7,31 +7,29 @@ const authRouter = express.Router();
 authRouter.post('/register', (req, res) => {
     const db = getDb();
     const { firstName, lastName, email, password, username } = req.body;
-    db.find_user_by_email([email])
+    db.find_user_by_email([ email ])
         .then( user => {
             if (user) {
                 console.error('There is already another account using that email address.');
                 return res.send({message: 'There is already an existing account using that email address.'});
             }
             else {
-                db.find_user_by_username([username])
+                db.find_user_by_username([ username ])
                     .then( user => {
                         if (user) {
                             console.error('That username already exists.');
                             return res.send({message: 'That username already exists.'});
                         }
                         else {
-                            db.register_user([firstName, lastName, email, password, username])
-                                .then( () => res.status(200).send())
+                            db.register_user([ firstName, lastName, email, password, username ])
+                                .then( promise => res.send(promise))
                                 .catch( err => res.send(err));
                         }
                     })
-                    .catch(err => {
-                        console.error('Error registering user.');
-                        res.send(err);
-                    });
+                    .catch(err => res.send(err));
             }
         })
+        .catch( err => res.send(err));
 });
 
 authRouter.post('/login', passport.authenticate('login'), (req, res) => {
