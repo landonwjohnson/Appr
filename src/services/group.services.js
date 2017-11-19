74b25ceeea55f3d9a2c4e6bbc3d24d5fff3c0ac5
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-19 10:27:30 -0700

    created create group service

--- src/services/group.services.js
+++ src/services/group.services.js
@@ -1,1 +1,10 @@
 import 'axios'
+
+const baseURL = '/api/group';
+
+function createGroup(body) {
+    return axios
+        .post(`${baseURL}/create`, body)
+        .then( res => res )
+        .catch( err => {throw err});
+}
