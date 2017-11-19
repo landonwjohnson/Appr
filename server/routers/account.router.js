f2be05c5fd3ca012f00591ff0af2f974ce050b7d
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-18 19:21:09 -0700

    created READ user router

--- server/routers/account.router.js
+++ server/routers/account.router.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const getDb = require('../database/bootstrap.database');
+
+const accountRouter = express.Router();
+
+accountRouter.get('/:userid', (req, res) => {
+    const db = getDb();
+    db.find_user_by_id([this.props.match.params.id])
+        .then( user => res.send(user))
+        .catch( err => res.send(err));
+});
+
+accountRouter.put('/update/:userid', (req, res) => {});
+
+accountRouter.delete('/delete/:userid', (req, res) => {});
+
+module.exports = accountRouter;
