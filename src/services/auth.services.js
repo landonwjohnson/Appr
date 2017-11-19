9cb87247ba75a25c00a72d56cac82883a3febab4
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-19 09:57:33 -0700

    create register service

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,1 +1,10 @@
 import 'axios';
+
+const baseURL = '/api/auth';
+
+function register(body) {
+    return axios
+        .post(`${baseURL}/register`, body)
+        .then( res => res )
+        .catch( err => {throw err});
+}
