593fb3f7bcd94c67093cb1979025d6afb908625a
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-19 08:32:46 -0700

    created create project route

--- server/routers/project.router.js
+++ server/routers/project.router.js
@@ -2,3 +2,11 @@ const express = require('express');
 const getDb = require('../database/bootstrap.database');
 
 const projectRouter = express.Router();
+
+projectRouter.post('/create', (req, res) => {
+    const db = getDb();
+    const { authorId } = req.body;
+    db.create_project([ authorId ])
+        .then( () => res.status(200).send())
+        .catch( err => res.send( err ));
+});
