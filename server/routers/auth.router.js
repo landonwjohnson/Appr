ddd2aae3d199bbf6d1cd7ff12d62572bd9561313
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-20 19:08:43 -0700

    fixed issue where a register request would always return 'email exists' error message.

--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -7,22 +7,20 @@ const authRouter = express.Router();
 authRouter.post('/register', (req, res) => {
     const { firstName, lastName, email, password, username } = req.body;
     const db = getDb();
-    db.find_user_by_email([ email ])
+    db.find_user_by_email(email)
         .then( user => {
-            if (user) {
-                console.error('There is already another account using that email address.');
-                return res.send({message: 'There is already an existing account using that email address.'});
+            if (user[0]) {
+                return res.status(409).send({message: 'There is already an existing account using that email address.'});
             }
             else {
-                db.find_user_by_username([ username ])
+                db.find_user_by_username(username)
                     .then( user => {
-                        if (user) {
-                            console.error('That username already exists.');
-                            return res.send({message: 'That username already exists.'});
+                        if (user[0]) {
+                            return res.status(409).send({message: 'That username already exists.'});
                         }
                         else {
                             db.register_user([ firstName, lastName, email, password, username ])
-                                .then( promise => res.send(promise))
+                                .then( promise => res.status(200).send())
                                 .catch( err => res.send(err));
                         }
                     })
