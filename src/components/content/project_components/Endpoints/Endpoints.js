f8813dae1d1d99165ad56d01576191e8c9aeb95a
Landon <landonwjohnson@gmail.com>
2017-11-24 12:54:35 -0700

    started to build the endpoints component

--- src/components/content/project_components/Endpoints/Endpoints.js
+++ src/components/content/project_components/Endpoints/Endpoints.js
@@ -9,15 +9,38 @@ class Endpoints extends Component {
       <div>
       <Header />
         <div className="project-view">
-          <ProjectSetupSidebar />
+          {/* <ProjectSetupSidebar /> */}
           <div className="endpoints-container">
-          <div className="endpoints-inner-container">
-        
-            <label className="project-section-header">Endpoints</label>
-            <div>
+          <label className="project-section-header">Endpoints</label>
+          <div className="endpoints-wrapper">
+            <div className="endpoints-inner-container">
+              
+              <div className="endpoints-list-container">
+                <div className="endpoint-item">
+                  
+                  <div className="endpoint-section-con">
+                    <div className="endpoint-listing-con">
+                      1.
+                    </div>
+                    <div className="ep-field-con">
+                        <input className="url-input-field" type="text" placeholder="URL" />
+                        <select className="http-verb-select">
+                          <option>GET</option>
+                          <option>POST</option>
+                          <option>UPDATE</option>
+                          <option>DELETE</option>
+                        </select>
+                    </div>
+                  
+                    
+                  </div>
+                </div>
+          
 
-            </div>
-            </div>
+
+              </div>
+              </div>
+            </div>  
               
           </div>
         </div>
