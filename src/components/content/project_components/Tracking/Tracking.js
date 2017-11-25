d44dc95017d257b37f2e7be9b26801facc18d6b2
Landon <landonwjohnson@gmail.com>
2017-11-25 13:10:35 -0700

    started working on the tracking component, the layout is looking better

--- src/components/content/project_components/Tracking/Tracking.js
+++ src/components/content/project_components/Tracking/Tracking.js
@@ -5,198 +5,164 @@ import Header from '../../../Header/Header';
 class Tracking extends Component {
   render() {
     return (
-      <div>
+      <div style={{"background": "grey", "height": "100vh", "overflow-y": "hidden"}}>
         <Header />
-        <div className="main-fix bg-grey">
+        <div className="main-fix">
         
         <div className="tracking-container">
-        <div className="project-header">
-            Appr
-          </div>
+        
           <div className="tracking-wrapper">
-         
+          <div className="project-header">
+            <label>Appr</label>
+          </div>
            
                 <div className="project-items-container">
-                  <div className="project-item">
-                    <div className="project-item-inner">
-                        <div className="prjt-item-header"> 
-                          <label> To Do </label>
+                        <div className="project-item">
+                              <div className="project-item-block">
+                                <div className="prjt-item-inner">
+                                    <div className="prjt-item-header"> 
+                                      <label> To Do </label>
+                                    </div>
+                                    <ul>
+                                      <li>Test</li>
+                                      <li>Test </li>
+                                    </ul>
+                              </div>
+                              </div>
                         </div>
-                        <ul>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                        </ul>
-                    </div>
-                  </div>
-                  <div className="project-item">
-                    <div className="project-item-inner">
-                        <h6> board test </h6>
-                        <ul>
-                          <li>Test</li>
-                          <li>Test </li>
-                          
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                        </ul>
-                    </div>
-                  </div>
-                  <div className="project-item">
-                    <div className="project-item-inner">
-                        <h6> board test </h6>
-                        <ul>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                        </ul>
-                    </div>
-                  </div>
-                  <div className="project-item">
-                    <div className="project-item-inner">
-                        <h6> board test </h6>
-                        <ul>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                        </ul>
-                    </div>
-                  </div>
-                  <div className="project-item">
-                    <div className="project-item-inner">
-                        <h6> board test </h6>
-                        <ul>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                        </ul>
-                    </div>
-                  </div>
-                  <div className="project-item">
-                    <div className="project-item-inner">
-                        <h6> board test </h6>
-                        <ul>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                        </ul>
-                    </div>
-                  </div>
-                  <div className="project-item">
-                    <div className="project-item-inner">
-                        <h6> board test </h6>
-                        <ul>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                         
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                        </ul>
-                    </div>
-                  </div>
-                  <div className="project-item">
-                    <div className="project-item-inner">
-                        <h6> board test </h6>
-                        <ul>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
-                          <li>Test</li>
-                          <li>Test </li>
-                          <li>Test</li>
+                        <div className="project-item">
+                              <div className="project-item-block">
+                                <div className="prjt-item-inner">
+                                    <div className="prjt-item-header"> 
+                                      <label> Backlog </label>
+                                    </div>
+                                    <ul>
+                                      <li>Test</li>
+                                      <li>Test </li>
+                                    </ul>
+                              </div>
+                              </div>
+                        </div>
+                        <div className="project-item">
+                              <div className="project-item-block">
+                                <div className="prjt-item-inner">
+                                    <div className="prjt-item-header"> 
+                                      <label> Repo </label>
+                                    </div>
+                                    <div className="prjt-item-body">
+                                      <ul>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+                                        <li>Test</li>
+                                        <li>Test </li>
+
+                                      </ul>
+                                      </div>
+                              </div>
+                              </div>
+                        </div>
+                        <div className="project-item">
+                              <div className="project-item-block">
+                                <div className="prjt-item-inner">
+                                    <div className="prjt-item-header"> 
+                                      <label> Routing </label>
+                                    </div>
+                                    <ul>
+                                      <li>Test</li>
+                                      <li>Test </li>
+                                    </ul>
+                              </div>
+                              </div>
+                        </div>
+                        <div className="project-item">
+                              <div className="project-item-block">
+                                <div className="prjt-item-inner">
+                                    <div className="prjt-item-header"> 
+                                      <label> Database </label>
+                                    </div>
+                                    <ul>
+                                      <li>Test</li>
+                                      <li>Test </li>
+                                    </ul>
+                              </div>
+                              </div>
+                        </div>
+                        <div className="project-item">
+                              <div className="project-item-block">
+                                <div className="prjt-item-inner">
+                                    <div className="prjt-item-header"> 
+                                      <label> Test </label>
+                                    </div>
+                                    <ul>
+                                      <li>Test</li>
+                                      <li>Test </li>
+                                    </ul>
+                              </div>
+                              </div>
+                        </div>
+                        <div className="project-item">
+                              <div className="project-item-block">
+                                <div className="prjt-item-inner">
+                                    <div className="prjt-item-header"> 
+                                      <label> Test </label>
+                                    </div>
+                                    <ul>
+                                      <li>Test</li>
+                                      <li>Test </li>
+                                    </ul>
+                              </div>
+                              </div>
+                        </div>
                         
-                        </ul>
-                    </div>
-                  </div>
+                       
+                      
                   
 
                 </div>
