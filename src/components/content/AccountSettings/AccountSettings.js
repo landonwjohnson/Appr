fe8f96ea38962372f336cb8a033ea93c46323c85
landonwjohnson <landonwjohnson@gmail.com>
2017-11-25 16:18:12 -0700

    edit profile modal working

--- src/components/content/AccountSettings/AccountSettings.js
+++ src/components/content/AccountSettings/AccountSettings.js
@@ -3,8 +3,41 @@ import editIcon from '../../../img/icons/Pencil-Icon.svg';
 import './account-settings.scss';
 import EditProfile from './modals/EditProfile/EditProfile';
 import Header from '../../Header/Header';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : 'auto',
+    marginLeft            : 'auto',
+    transform             : 'translate(0%, 40%)'
+  }
+};
+
 
 class AccountSettings extends Component {
+  constructor(){
+    super()
+    this.state={
+      modalIsOpen: false
+    }
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  
+
   render() {
     return (
       <div>
@@ -20,11 +53,11 @@ class AccountSettings extends Component {
                     <div className="headline-22">Landon Johnson </div>
                     <div className="text-9">@landonwjohnson</div>
                 </div>
-                <div className="edit-profile-btn">
+                <div className="edit-profile-btn" onClick={this.openModal}>
                   <div className="button-icon"><img src={editIcon}/></div>
                   Edit Profile
                   </div>
-              </div>
+                </div>
             </div>
           </div>
           <div className="account-section">
@@ -36,8 +69,15 @@ class AccountSettings extends Component {
               <div className="headline-20"> Change Avatar </div>
             </div>
           </div>
-
-          {/* <EditProfile /> */}
+          <Modal 
+            isOpen ={this.state.modalIsOpen} 
+            onRequestClose={this.closeModal}
+            className="modal-account-settings-content"
+            overlayClassName="modalStyle"
+            style={customStyles}
+          >
+            <EditProfile onCloseBtnClick={this.closeModal} />
+          </Modal>
       </div>
       </div>
     );
