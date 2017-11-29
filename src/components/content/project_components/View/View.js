d70a47486ed81c2f4b03c06309145cd2dfeb2706
goborn <garry.oborn@365smartshop.com>
2017-11-28 19:33:06 -0700

    re-added view.js and view.scss

--- src/components/content/project_components/View/View.js
+++ src/components/content/project_components/View/View.js
@@ -8,13 +8,38 @@ class View extends Component {
     return (
       <div>
       <Header />
-        <div>
+        <div className="main-fix">
           <ProjectSetupSidebar />
-          <div className="view-container">
-              
+          <div className="view-inner-con">
+          <div className="view-top-label">
+            <h1 className="project-label-name">View</h1>
+              <div className="view-input-con">
+                <div className="view-input-individual">
+                  <label className="view-num-individual">1.</label>
+                    <input className="view-input-name" placeholder="Name" type="text" />
+                    <input className="view-input-imgurl" placeholder="Image URL" type="text" />
+                    <button className="button">Save</button>
+                </div>
+                <div className="view-input-individual">
+                  <label className="view-num-individual">2.</label>
+                    <input className="view-input-name" placeholder="Name" type="text" />
+                    <input className="view-input-imgurl" placeholder="Image URL" type="text" />
+                    <button className="button">Save</button>
+                </div>
+                <div className="view-input-individual">
+                  <label className="view-num-individual">3.</label>
+                    <input className="view-input-name" placeholder="Name" type="text" />
+                    <input className="view-input-imgurl" placeholder="Image URL" type="text" />
+                    <button className="button">Save</button>
+                </div>
+                <div className="view-add">
+                  <a href="">+Add</a>
+                </div>
+              </div>
           </div>
         </div>
       </div>
+    </div>
     );
   }
 }
