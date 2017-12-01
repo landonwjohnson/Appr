3428e6e9d75e0dc402cc28eb32244a956b155701
goborn <garry.oborn@365smartshop.com>
2017-11-30 19:44:07 -0700

    Added post request to project.idea.router.js

--- server/routers/project.Idea.router.js
+++ server/routers/project.Idea.router.js
@@ -3,9 +3,20 @@ const getDb = require('../database/bootstrap.database');
 
 const projectIdeaRouter = express.Router();
 
-projectIdeaRouter.post('/create', (req, res) => {
-    const { /*add db columns here */} = req.body;
+projectIdeaRouter.post('project/create/idea', (req, res) => {
+    const { projectId, ideaData } = req.body;
     const db = getDb();
-    db
+    db.read_project_idea([ ideaData ])
+        .then( projectIdea => {
+            if (projectIdea[0] && projectIdea[0].ideaData === ideaData ) {
+                return res.status(409).send({message: 'Idea is a duplicate, please choose another name'});
+            }
+            else {
+                db.create_project_idea([ projectId, ideaData ])
+                .then( promise => res.send())
+                .catch( err => res.send(err));
+            }
+        })
+        .catch( err => res.send(err));
+});
 
-})
