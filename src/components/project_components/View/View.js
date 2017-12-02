8ee3b46305a7e634623cb22cb7fc678823dff066
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-12-02 10:13:05 -0700

    resolved merge conflict

--- src/components/project_components/View/View.js
+++ src/components/project_components/View/View.js
@@ -6,33 +6,6 @@ import './view.scss';
 class View extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="view-body">
-          <Header />
-
-          <div className="view-wrapper">
-              <Nav_Sidebar />
-
-              <div className="view-area">
-                  <h1>View</h1>
-                  <div className="view-input-one">
-                        <h3>1.</h3>
-                        <input placeholder="Name" className="view-input-name"/>
-                        <input placeholder="Image URL" className="view-input-img-url"/>
-                        <button>Save</button>
-                  </div>
-                  <div className="view-input-two">
-                        <h3>2.</h3>
-                        <input placeholder="Name" className="view-input-name"/>
-                        <input placeholder="Image URL" className="view-input-img-url"/>
-                        <button>Save</button>
-                  </div>
-                  <div className="view-add">
-                      <a href="">+Add</a>
-                  </div>
-              </div>
-          </div>
-=======
       <div className="view-container">
         < Header />
         <div className="view-inner-con">
@@ -57,7 +30,6 @@ class View extends Component {
               </div>
           </div>
         </div>
->>>>>>> ae25d620e852c826fdf5be75ac39a1f2360d41fc
       </div>
     );
   }
