f232cf99540e1154bf2bd95c50dda8f7f86be3df
goborn <garry.oborn@365smartshop.com>
2017-12-02 12:38:03 -0700

    fixed issue with create_project_endpoint.sql file

--- db/create_project_endpoint.sql
+++ db/create_project_endpoint.sql
@@ -1,3 +1,3 @@
-INSERT INTO project_endpoint (url_data, is_get, is_post, is_update, is_delete)
-VALUES ($1, $2, $3, $4, $5)
+INSERT INTO project_endpoint (project_id, url_data, is_get, is_post, is_update, is_delete, req_endpoint_id, res_endpoint_id)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 ;
