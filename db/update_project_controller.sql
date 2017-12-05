efb7306c73e652d9be4e806cd1b65aea36ab3301
goborn <garry.oborn@365smartshop.com>
2017-12-05 16:18:09 -0700

    Removed project_id from update sql queries

--- db/update_project_controller.sql
+++ db/update_project_controller.sql
@@ -1,4 +1,4 @@
 UPDATE  project_controller
-SET project_id = $2, when_data = $3, do_data = $4, require_data = $5
+SET when_data = $3, do_data = $4, require_data = $5
 WHERE id = $1 AND project_id = $2
 ;
