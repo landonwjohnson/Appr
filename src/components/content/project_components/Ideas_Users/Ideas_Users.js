ec103bc4c08cb4af3cc5a72eee0c8afd6f9a346d
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2017-12-08 15:04:43 -0700

    cleaned up the return of the Ideas_Users component

--- src/components/content/project_components/Ideas_Users/Ideas_Users.js
+++ src/components/content/project_components/Ideas_Users/Ideas_Users.js
@@ -34,7 +34,7 @@ class Ideas_Users extends Component {
             return(
                 <div className="users-item" key={getAltUId()}>
                     <section>
-                    <label>{(users[user] + 1) + '.'}</label>
+                        <label>{(users[user] + 1) + '.'}</label>
                         <input value={}></input>
                         <input value={}></input>
                         <input value={}></input>
@@ -45,53 +45,44 @@ class Ideas_Users extends Component {
             )
         });
 
-    return (
-      <div>
-      <Header />
-      <div className="main-fix">
-       <ProjectSetupSidebar />
-
-          <div className="ideasUsers-container">
-            <div className="ideasUsers-wrapper">
-             <div className="project-section-header">
-                <label>Ideas & Users</label>
-            </div>
-                    <div className="ideas-users-area drop-shadow">
-                        <div className="ideas-users-wrapper">
-                          <div className="area-title">Ideas</div>
-                          <div className="ideas-list">
-
-                           {displayIdeas}
-
-                          </div>
-                          <div className="ideas-users-footer">
-                              <button className="add-button" onClick={this.addIdeaItemHandler}> <span/> Add Idea </button>
-                          </div>
-                          </div>
-                    </div>
-
-                    <div className="ideas-users-area drop-shadow">
-                        <div className="ideas-users-wrapper">
-                        <div className="area-title">Users</div>
-                        <div className="users-list">
-
-                            {displayUsers}
-
-                          </div>
-                          <div className="ideas-users-footer">
-                              <button className="add-button" onClick={this.addUserItemHandler}> <span/> Add User </button>
-                          </div>
+        return (
+            <div>
+                <Header />
+                <div className="main-fix">
+                    <ProjectSetupSidebar />
+                    <div className="ideasUsers-container">
+                        <div className="ideasUsers-wrapper">
+                            <div className="project-section-header">
+                                <label> Ideas & Users </label>
+                            </div>
+                            <div className="ideas-users-area drop-shadow">
+                                <div className="ideas-users-wrapper">
+                                    <div className="area-title"> Ideas </div>
+                                    <div className="ideas-list">
+                                        {displayIdeas}
+                                    </div>
+                                    <div className="ideas-users-footer">
+                                        <button className="add-button" onClick={this.addIdeaItemHandler}> <span/> Add Idea </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="ideas-users-area drop-shadow">
+                                <div className="ideas-users-wrapper">
+                                    <div className="area-title">Users</div>
+                                    <div className="users-list">
+                                        {displayUsers}
+                                    </div>
+                                    <div className="ideas-users-footer">
+                                        <button className="add-button" onClick={this.addUserItemHandler}> <span/> Add User </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-             
-
+                </div>
             </div>
-        </div>
-      </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Ideas_Users;
