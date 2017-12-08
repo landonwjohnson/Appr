67eb6d0d206eedca62f0b5b57748bf53b9953631
goborn <garry.oborn@365smartshop.com>
2017-12-07 20:59:05 -0700

    Fixed create endpoint router

--- server/routers/project.endpoint.router.js
+++ server/routers/project.endpoint.router.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const getDB = require('../database/bootstrap.database');
+const getDb = require('../database/bootstrap.database');
 
 const projectEndpointRouter = express.Router();
 
 projectEndpointRouter.post('/:projectid/create/endpoint', (req, res) => {
     const projectid = req.params.projectid;
-    const {urlData, isGet, isPost, isUpdate, isDelete} = req.body;
-    const db = getDB;
-    db.create_project_endpoint( [projectid, urlData, isGet, isPost, isUpdate, isDelete] )
+    const {urlData, isGet, isPost, isUpdate, isDelete, reqEnpointId, resEndpointId} = req.body;
+    const db = getDb();
+    db.create_project_endpoint( [projectid, urlData, isGet, isPost, isUpdate, isDelete, reqEnpointId, resEndpointId] )
         .then(promise => res.send())
         .catch(err => res.send(err));
 })
@@ -15,7 +15,7 @@ projectEndpointRouter.post('/:projectid/create/endpoint', (req, res) => {
 projectEndpointRouter.get('/:projectid/endpoint/:endpointid', (req, res) => {
     const projectid = req.params.projectid;
     const endpointid = req.params.endpointid;
-    const db = getDB;
+    const db = getDb();
     db.find_project_endpoint([projectid, endpointid])
         .then(endpoint => res.send(endpointid))
         .catch(err => res.send(err));
@@ -25,7 +25,7 @@ projectEndpointRouter.put('/:projectid/update/endpoint/:endpointid', (req, res) 
     const projectid = req.params.projectid;
     const endpointid = req.params.endpointid;
     const {urlData, isGet, isPost, isUpdate, isDelete} = req.body;
-    const db = getDB;
+    const db = getDb();
     db.update_project_endpoint([projectid, endpointid, urlData, isGet, isPost, isUpdate, isDelete])
         .then(promise => res.send())
         .catch(err => res.send(err));
@@ -34,7 +34,7 @@ projectEndpointRouter.put('/:projectid/update/endpoint/:endpointid', (req, res) 
 projectEndpointRouter.delete('/:projectid/delete/endpoint/:endpointid', (req, res) => {
     const projectid = req.params.projectid;
     const endpointid = req.params.endpointid;
-    const db = getDB;
+    const db = getDb();
     db.delete_project_endpoint([projectid, endpointid])
         .then(promise => res.send())
         .catch(err => res.send(err));
