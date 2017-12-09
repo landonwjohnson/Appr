35e03c7b8e6a0bfdbcc1bfce6810189e9904e8ee
Austin <10798487@my.uvu.edu>
2017-12-09 12:53:22 -0700

    put color on the componants

--- src/components/content/project_components/View/View.js
+++ src/components/content/project_components/View/View.js
@@ -6,6 +6,7 @@ import { createProjectView, findProjectViews, findProjectView, updateProjectView
 import './view.scss';
 
 class View extends Component {
+<<<<<<< HEAD
     constructor(props) {
       super(props);
       this.state={
@@ -112,6 +113,57 @@ class View extends Component {
         </div>
         )
     });
+=======
+  constructor(props){
+  super(props);
+  this.state={
+      view: {
+          arr:[{
+              key: 1,
+              label: 1,
+              name: 'Name',
+              imageurl: 'ImageURL',
+          }]
+      }
+    }
+    this.addViewItemHandler = this.addViewItemHandler.bind(this);
+    this.removeViewItemHandler = this.removeViewItemHandler.bind(this);
+  }
+
+  addViewItemHandler(){
+    let ViewList = this.state.view.arr;
+    ViewList.push({
+      key: 2,
+      label: 2,
+      name: 'Name',
+      imageurl: 'ImageURL',
+    })
+    this.setState({arr: ViewList})
+  }
+
+  removeViewItemHandler(){
+    let ViewList = this.state.view.arr;
+    ViewList.splice(1, 1);
+    this.setState({arr: ViewList})
+  }
+
+
+  
+  render() {
+    const displayViews = this.state.view.arr.map( view => {
+      return(
+        <div className="view-item">
+        <section>
+          <label>{view.label + '.'}</label>
+          <input className="view-input-name" placeholder="Name" type="text" />
+          <input className="view-input-imgurl" placeholder="Image URL" type="text" />
+        </section>
+          <button className="not-enough-info-btn">Save</button>
+          <span className="delete-x" onClick={this.removeViewItemHandler}>&times;</span> 
+      </div>
+      )
+    })
+>>>>>>> 6ea2d610... put color on the componants
 
     return (
       <div>
@@ -130,7 +182,11 @@ class View extends Component {
                   {displayViews}
               
                   <div className="view-footer">
+<<<<<<< HEAD
                   <button className="add-button" onClick={this.handleAddViewButton}> <span/> Add View </button>
+=======
+                  <button className="add-button" onClick={this.addViewItemHandler}> <span/> Add View </button>
+>>>>>>> 6ea2d610... put color on the componants
                   </div>
                 </div>
               </div>
