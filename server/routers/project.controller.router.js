6632545412255dfdb76ad58bbb776f83b48e929e
goborn <garry.oborn@365smartshop.com>
2017-12-09 09:07:40 -0700

    added project controller router

--- server/routers/project.controller.router.js
+++ server/routers/project.controller.router.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const getDB = require('../database/bootstrap.database.js');
+const getDb = require('../database/bootstrap.database.js');
 
 const projectControllerRouter = express.Router();
 
 projectControllerRouter.post('/:projectid/create/controller', (req, res) => {
     const projectid = req.params.projectid;
     const { whenData, doData, requireData } = req.body;
-    const db = getDB();
+    const db = getDb();
     db.create_project_controller( [projectid, whenData, doData, requireData] )
         .then( promise => res.send() )
         .catch( err => res.send(err) );
 })
 
+projectControllerRouter.get('/:projectid/controller', (req, res) => {
+    const projectid = req.params.projectid;
+    const db = getDb();
+    db.find_project_endpoints([ projectid ])
+        .then( projectControllers => res.send(projectControllers))
+        .catch( err => res.send(err));
+})
+
 projectControllerRouter.get('/:projectid/controller/:controllerid', (req, res) => {
     const projectid = req.params.projectid;
     const controller = req.params.controllerid;
-    const db = getDB();
+    const db = getDb();
     db.find_project_controller( [projectid, controllerid] )
         .then( controller => res.send(controller) )
         .catch( err => res.send(err) );
@@ -25,7 +33,7 @@ projectControllerRouter.put('/:projectid/update/controller/:controllerid', (req,
     const projectid = req.params.projectid;
     const controller = req.params.controllerid;
     const { whenData, doData, requireData } = req.body;
-    const db = getDB;
+    const db = getDb();
     db.update_project_controller( [projectid, controllerid, whenData, doData, requireData] )
         .then( promise => res.send() )
         .catch( err => res.send(err) );
@@ -34,7 +42,7 @@ projectControllerRouter.put('/:projectid/update/controller/:controllerid', (req,
 projectControllerRouter.delete('/:projectid/delete/controller/:controllerid', (req, res) => {
     const projectid = req.params.projectid;
     const controller = req.params.controllerid;
-    const db = getDB;
+    const db = getDb();
     db.delete_project_controller( [projectid, controllerid] )
         .then( promise => res.send() )
         .catch( err => res.send(err) );
