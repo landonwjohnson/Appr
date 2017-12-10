f9dda5be5b760007a02ce962354d83372ba312a1
Landon <landonwjohnson@gmail.com>
2017-12-10 16:35:06 -0700

    creating a feed back modal

---
+++ src/components/content/Feedback/Feedback.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+
+class Feedback extends Component {
+  render() {
+      const getURLPath = `${window.location.href}`
+    return (
+        <div className="modalStyle-inner">
+        <div className="modal-account-settings-content">
+            <div className="modal-header">
+                <div className="modal-header-placeholder"></div>
+                <h2 className="modal-title">Report Bug</h2>
+                <span className="closeBtn" onClick={this.props.onCloseBtnClick}>&times;</span>
+            </div>
+            <form method="post" action="/send">
+            <div className="modal-body">
+                <label class="modal-input-tag">Problem</label>
+                <section class="modal-row">
+                    <input type="text" name="problem" id="problem" class="modal-form" required />
+                </section>
+                <label class="modal-input-tag">Description</label>
+                <section class="modal-row">
+                    <textarea id="description" name="description" class="modal-form" style={{"min-height": "70px", "resize": "none"}} required/>
+                </section>
+                <input type="text" name="location" id="location" value={getURLPath} style={{"display": "none"}} />
+                    
+            </div>
+            <div className="submitModal">
+                <button type="submit">
+                hello
+                </button>
+            </div>
+            </form>
+        </div>
+    </div>
+    );
+  }
+}
+
+export default Feedback;
