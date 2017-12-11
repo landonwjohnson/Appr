82360a84597f8492d4981ba4849ba7d4d1ba52cd
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2017-12-10 21:51:38 -0700

    set scaffolding for the dashboard endpoint

--- src/components/content/Dashboard/Dashboard.js
+++ src/components/content/Dashboard/Dashboard.js
@@ -6,42 +6,50 @@ import addIcon from '../../../img/icons/add-icon.svg';
 import Header from '../../Header/Header'
 
 class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-      <Header />
-      <div className="dashboard-container">
-      <div className="personal-list-container">
-          <label className="dash-section-title">Personal Projects</label>
-          <ul className="projects-list">
-              {/* <li className="project-thumb">
-                  <div className="project-thumb-body">
-                      <div className="project-thumb-icon"></div>
-                  </div>
-                  <div className="project-thumb-footer">
-                      <label>Houser</label>
-                  </div>
-              </li>
-               */}
-              <Link to="/ideas">
-                  <li className="create-project-thumb">
-                      <div className="create-project-thumb-body">
-                          <img src={addIcon} alt="add icon"/>
-                      </div>
-                      <div className="create-project-thumb-footer">
-                          <label>Create New Project</label>
-                      </div>
-                  </li>
-              </Link>
-          </ul>
-      </div>
-      </div>
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
+	render() {
+		
+		return (
+			<div>
+				<Header />
+				<div className="dashboard-container">
+					<div className="group-list-container">
+						<label className="dash-section-title">Groups</label>
+						<ul className="groups-list">
 
-</div>
+							{/* groups render here */}
+							{/* create a group renders here */}
 
-    );
-  }
+						</ul>
+					</div>
+					<div className="personal-list-container">
+						<label className="dash-section-title">Personal Projects</label>
+						<ul className="projects-list">
+
+							{/* projects render here */}
+
+							<Link to="/ideas">
+								<li className="create-project-thumb">
+									<div className="create-project-thumb-body">
+										<img src={addIcon} alt="add icon"/>
+									</div>
+									<div className="create-project-thumb-footer">
+										<label>Create New Project</label>
+									</div>
+								</li>
+							</Link>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+
+		);
+	}
 }
 
 export default Dashboard;
