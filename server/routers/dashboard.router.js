265c5f384f1f952e7f8539ad70eb60697c423408
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2017-12-10 18:26:48 -0700

    built out dashboard router to grab all project and group info for a user

--- server/routers/dashboard.router.js
+++ server/routers/dashboard.router.js
@@ -4,13 +4,48 @@ const getDb = require('../database/bootstrap.database');
 const dashboardRouter = express.Router();
 
 dashboardRouter.get('/:userid', (req, res) => {
-    const userId = this.props.match.params.userid;
+    const userid = req.params.userid;
     const db = getDb();
-    db.find_usergroups_by_userid([ userId ])
-        .then( groups => {
-            db.find_userprojects_by_userid([ userId ])
-                .then(projects => res.send(groups, projects))
+    db.find_usergroups_by_userid([ userid ])
+        .then( ugroupids => {
+            let userGroups = [];
+            let groupProjects = [];
+            let userProjects = [];
+            for (var i in ugroupids) {
+                db.find_group_by_id([ i ])
+                    .then( group => {
+                        userGroups.push(group[0]);
+                        db.find_groupprojects_by_groupid([ i ])
+                            .then( gprojectids => {
+                                for (var j in gprojectids) {
+                                    db.find_project_by_id([ j ])
+                                        .then( gproject => {
+                                            groupProjects.push(gproject[0]);
+                                            res.send();
+                                        })
+                                        .catch(err => res.status(500).send(err));
+                                }
+                                res.send();
+                            })
+                            .catch(err => res.status(500).send(err));
+                        res.send();
+                    })
+                    .catch(err => res.status(500).send(err));
+            }
+            db.find_userprojects_by_userid([ userid ])
+                .then( uprojectids => {
+                    for (var k in uprojectids) {
+                        db.find_project_by_id([ k ])
+                            .then( uproject => {
+                                userProjects.push(uproject[0]);
+                                res.send();
+                            })
+                            .catch(err => res.status(500).send(err));
+                    }
+                    res.send();
+                })
                 .catch(err => res.status(500).send(err));
+            res.send(userGroups, groupProjects, userProjects)
         })
         .catch(err => res.status(500).send(err));
 });
