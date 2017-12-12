4a5af76448a2bf340130df845c62f8d9c1b576c9
Landon <landonwjohnson@gmail.com>
2017-12-12 13:00:36 -0700

    working on feedback modal

--- server/feedbackMailerTest.js
+++ server/feedbackMailerTest.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-const feedbackMailer = express();
+const feedbackMailer = express.Router();
 
 
 feedbackMailer.post('/reportbug', (req, res) => {
