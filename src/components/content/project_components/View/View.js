f8182a92aa90577792ad7cef36d8a22a2e67a6a3
Austin <10798487@my.uvu.edu>
2017-12-12 19:53:14 -0700

    solved merge conflicts

--- src/components/content/project_components/View/View.js
+++ src/components/content/project_components/View/View.js
@@ -6,19 +6,6 @@ import { createProjectView, findProjectViews, findProjectView, updateProjectView
 import './view.scss';
 
 class View extends Component {
-<<<<<<< HEAD
-  constructor(props){
-  super(props);
-  this.state={
-      view: {
-          arr:[{
-              key: 1,
-              label: 1,
-              name: 'Name',
-              imageurl: 'ImageURL',
-          }]
-      }
-=======
     constructor(props) {
       super(props);
       this.state={
@@ -75,30 +62,8 @@ class View extends Component {
                 }
             })
             .catch(err => {throw err});
->>>>>>> 92fc274aeb98ea39bc68cc213623215c2496f6e3
     }
-    this.addViewItemHandler = this.addViewItemHandler.bind(this);
-    this.removeViewItemHandler = this.removeViewItemHandler.bind(this);
-  }
 
-<<<<<<< HEAD
-  addViewItemHandler(){
-    let ViewList = this.state.view.arr;
-    ViewList.push({
-      key: 2,
-      label: 2,
-      name: 'Name',
-      imageurl: 'ImageURL',
-    })
-    this.setState({arr: ViewList})
-  }
-
-  removeViewItemHandler(){
-    let ViewList = this.state.view.arr;
-    ViewList.splice(1, 1);
-    this.setState({arr: ViewList})
-  }
-=======
     handleChangeView(e, index, field) {
         const newState = this.state.views;
         newState[index][field] = e.target.value;
@@ -130,26 +95,17 @@ class View extends Component {
         const newState = this.state.views;
         newState.splice(index, 1);
         this.setState({ views: newState });
->>>>>>> 92fc274aeb98ea39bc68cc213623215c2496f6e3
 
+        deleteProjectView(projectid, viewid)
+            .then(res => {
+                if(res.status !== 200) {
+                    console.log(res);
+                }
+            })
+            .catch(err => {throw err});
+    }
 
-  
   render() {
-<<<<<<< HEAD
-    const displayViews = this.state.view.arr.map( view => {
-      return(
-        <div className="view-item">
-        <section>
-          <label>{view.label + '.'}</label>
-          <input className="view-input-name" placeholder="Name" type="text" />
-          <input className="view-input-imgurl" placeholder="Image URL" type="text" />
-        </section>
-          <button className="not-enough-info-btn">Save</button>
-          <span className="delete-x" onClick={this.removeViewItemHandler}>&times;</span> 
-      </div>
-      )
-    })
-=======
     const views = this.state.views;
     const displayViews = views.map( view => {
         const index = views.indexOf(view);
@@ -165,7 +121,6 @@ class View extends Component {
         </div>
         )
     });
->>>>>>> 92fc274aeb98ea39bc68cc213623215c2496f6e3
 
     return (
       <div>
@@ -184,7 +139,7 @@ class View extends Component {
                   {displayViews}
               
                   <div className="view-footer">
-                  <button className="add-button" onClick={this.addViewItemHandler}> <span/> Add View </button>
+                  <button className="add-button" onClick={this.handleAddViewButton}> <span/> Add View </button>
                   </div>
                 </div>
               </div>
