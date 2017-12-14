00fa6accf04124628dcb081a66dd4a7904513394
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-12-13 18:13:41 -0700

    projectsetupsidebar renders and changes the name of the project

--- src/components/content/project_components/ProjectSetupSidebar/ProjectSetupSidebar.js
+++ src/components/content/project_components/ProjectSetupSidebar/ProjectSetupSidebar.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './project-setup-sidebar.scss';
+import { findProject, updateProject } from '../../../../services/project.services'
 
 export default class ProjectSetupSidebar extends Component {
-    render() {
-        const userid = this.props.userid;
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.handleChangeName = this.handleChangeName.bind(this);
+    }
+
+    componentWillMount() {
         const projectid = this.props.projectid;
+        findProject(projectid)
+            .then( res => {
+                if (res.status !== 200) {
+                    console.log(res);
+                }
+                else {
+                    this.setState(res.data[0]);
+                }
+            })
+            .catch(err => {throw err});
+    }
+
+    handleChangeName(e) {
+        const projectid = this.props.projectid;
+        const reqBody = { name: e.target.value };
+        this.setState({ name: e.target.value });
+        updateProject(projectid, reqBody)
+            .then(res => res)
+            .catch(err => {throw err});
+    }
+
+    render() {
+        const { userid, projectid } = this.props;
         return (
             <div className="project-sidebar-container">
             <div className="project-sidebar-header">
-                <input type="text" placeholder="Project Name" autoFocus/> 
+                <input type="text" value={this.state.name} onChange={e => this.handleChangeName(e)} autoFocus/> 
             </div>
             <ul className="project-sidebar-inner">
                 <Link to={`/user/${userid}/project/${projectid}/ideas`} className="psb-item"><li>Ideas & Users</li></Link>
