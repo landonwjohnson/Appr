ad5677a6e387bb5cef0ea1a7a1f14c49768dc207
Landon <landonwjohnson@gmail.com>
2017-12-15 16:36:42 -0700

    finishing setting up background side menu, I also adding toggle functionality

--- src/components/content/project_components/ProjectSetupSidebar/ProjectSetupSidebar.js
+++ src/components/content/project_components/ProjectSetupSidebar/ProjectSetupSidebar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 import './project-setup-sidebar.scss';
 import { findProject, updateProject } from '../../../../services/project.services';
 import HotSpringsUtah from '../../../../img/backgrounds/Hot-Springs-Utah.jpg';
@@ -13,13 +14,10 @@ export default class ProjectSetupSidebar extends Component {
             id: 1,
             name: "Defeat the Empire",
             status_id: 1,
-
-            UI:{
-                showBackgroundList: false
-            }
-           
+            UI: {backgroundMenu: false}
         };
         this.handleChangeName = this.handleChangeName.bind(this);
+        this.toggleBackgroundMenu = this.toggleBackgroundMenu.bind(this);
     }
 
     componentWillMount() {
@@ -45,8 +43,37 @@ export default class ProjectSetupSidebar extends Component {
             .catch(err => {throw err});
     }
 
+    toggleBackgroundMenu(){
+        console.log('Background Menu')
+        if(this.state.UI.backgroundMenu === false){
+            this.setState({UI: {backgroundMenu: true}})
+        }else{
+            this.setState({UI: {backgroundMenu: false}})
+        }
+    }
+
     render() {
-        console.log(this.state)
+        let projectHeaderClass = classnames({
+            "project-sidebar-header": this.state.UI.backgroundMenu,
+            "project-background-header": this.state.UI.backgroundMenu
+           
+            
+            
+        })
+
+        let projectListClass = classnames({
+            "nav-list": this.state.UI.backgroundMenu,
+            "background-list": this.state.UI.backgroundMenu
+            
+        })
+
+        let projectFooterClass = classnames({
+            "project-sidebar-footer": this.state.UI.backgroundMenu,
+            "project-background-footer": this.state.UI.backgroundMenu
+            
+  
+        })
+
         const { userid, projectid } = this.props;
         return (
             <div className="project-sidebar-container">
@@ -54,43 +81,45 @@ export default class ProjectSetupSidebar extends Component {
                      <label> Back </label>
                      <input type="text" value={this.state.name} onChange={e => this.handleChangeName(e)} autoFocus/> 
                  </div> */}
-
-                <div className="project-sidebar-header-bg">
-                    <input className="rename-project" type="text" value={this.state.name} onChange={e => this.handleChangeName(e)} autoFocus style={{'display': 'none'}}/> 
+                 <form>
+                <div className={`project-sidebar-header ${projectHeaderClass}`}>
+                    <input className="rename-project" type="text" value={this.state.name} onChange={e => this.handleChangeName(e)} autoFocus /> 
                     <input className="change-background-url" type="url" placeholder="Enter Image URL" pattern={/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)jpg|png|gif?/g}/>
                 </div>
                 
-                <ul className="project-sidebar-inner" style={{'display': 'none'}}>
-                    <Link to={`/user/${userid}/project/${projectid}/ideas`} className="psb-item"><li>Ideas & Users</li></Link>
-                    <Link to={`/user/${userid}/project/${projectid}/features`} className="psb-item"><li >Features</li></Link>
-                    <Link to={`/user/${userid}/project/${projectid}/views`}  className="psb-item"><li>Views</li></Link>
-                    <Link to={`/user/${userid}/project/${projectid}/controllers`} className="psb-item"><li>Controllers</li></Link>
-                    <Link to={`/user/${userid}/project/${projectid}/endpoints`} className="psb-item"><li>Endpoints</li></Link>
-                    <Link to={`/user/${userid}/project/${projectid}/schema`} className="psb-item"><li >Schema</li></Link>
-                    <Link to={`/user/${userid}/project/${projectid}/tracker`} className="psb-item"><li >Tracker</li></Link>
+                <ul className={`nav-list ${projectListClass} `} >
+                    <Link to={`/user/${userid}/project/${projectid}/ideas`} className="nav-item"><li>Ideas & Users</li></Link>
+                    <Link to={`/user/${userid}/project/${projectid}/features`} className="nav-item"><li >Features</li></Link>
+                    <Link to={`/user/${userid}/project/${projectid}/views`}  className="nav-item"><li>Views</li></Link>
+                    <Link to={`/user/${userid}/project/${projectid}/controllers`} className="nav-item"><li>Controllers</li></Link>
+                    <Link to={`/user/${userid}/project/${projectid}/endpoints`} className="nav-item"><li>Endpoints</li></Link>
+                    <Link to={`/user/${userid}/project/${projectid}/schema`} className="nav-item"><li >Schema</li></Link>
+                    <Link to={`/user/${userid}/project/${projectid}/tracker`} className="nav-item"><li >Tracker</li></Link>
+
+
+                    {/*Background Menu Items*/}
+                        <li className="no-background-box"><a> <label>No Background</label> </a> </li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
+                        <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
                 </ul>
 
-                <ul className="background-list">
-                
-                    <li className="no-background-box"><a> <label>No Background</label> </a> </li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${HotSpringsUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section> </li>
-                    <li className="background-box" style ={ { backgroundImage: `url(${ThistleHouseUtah})` } }> <section className="attribution"><Link to="http://bit.ly/landonwjohnson-on-behance" className="attribution" target="_blank"><label>Landon Johnson</label></Link></section></li>
-                    
-                </ul>
-                <div className="project-side-bar-footer">
-                    <label>Change Background</label>
-                    <div className="project-side-bar-buttonset">
-                        <button>Save</button>
+                <div className={`project-sidebar-footer ${projectFooterClass}`}>
+                    <label onClick={this.toggleBackgroundMenu}>Change Background</label>
+                    <div className="project-sidebar-buttonset">
+                        <button className="changeBackgroundBtn">Change</button>
+                        <div onClick={this.toggleBackgroundMenu} className="closeBackgroundList"> <span /> </div>
                     </div>
+                    
                 </div>
+                </form>
             </div>
         )
     }
