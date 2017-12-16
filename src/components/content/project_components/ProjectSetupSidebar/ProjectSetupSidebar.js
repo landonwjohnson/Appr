a5ae0cbde97d798386bd2fefa905fbca38ebb25f
landonwjohnson <landonwjohnson@gmail.com>
2017-12-16 09:52:11 -0700

    updated sidebar state with project property

--- src/components/content/project_components/ProjectSetupSidebar/ProjectSetupSidebar.js
+++ src/components/content/project_components/ProjectSetupSidebar/ProjectSetupSidebar.js
@@ -10,6 +10,7 @@ export default class ProjectSetupSidebar extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            project: {},
             UI: {backgroundMenu: false}
         };
         this.handleChangeName = this.handleChangeName.bind(this);
@@ -24,7 +25,7 @@ export default class ProjectSetupSidebar extends Component {
                     console.log(res);
                 }
                 else {
-                    this.setState(res.data[0]);
+                    this.setState({ project: res.data[0] });
                 }
             })
             .catch(err => {throw err});
@@ -33,7 +34,9 @@ export default class ProjectSetupSidebar extends Component {
     handleChangeName(e) {
         const projectid = this.props.projectid;
         const reqBody = { name: e.target.value };
-        this.setState({ name: e.target.value });
+        const newState = this.state.project;
+        newState.name = e.target.value;
+        this.setState({ project: newState });
         updateProject(projectid, reqBody)
             .then(res => res)
             .catch(err => {throw err});
@@ -69,7 +72,7 @@ export default class ProjectSetupSidebar extends Component {
             <div className="project-sidebar-container">
                  <form>
                 <div className={`project-sidebar-header ${projectHeaderClass}`}>
-                    <input className="rename-project" type="text" value={this.state.name} onChange={e => this.handleChangeName(e)} autoFocus /> 
+                    <input className="rename-project" type="text" value={this.state.project.name} onChange={e => this.handleChangeName(e)} autoFocus /> 
                     <input className="change-background-url" type="url" placeholder="Enter Image URL" pattern={/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)jpg|png|gif?/g}/>
                 </div>
                 
