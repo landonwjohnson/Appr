c1b355620a54995dc4b8171a98b623c09cceb87d
Austin <10798487@my.uvu.edu>
2017-12-19 20:45:59 -0700

    passed down handleDeleteController and handleChangeInput through props from ControllerField.js to Controller.js

--- src/components/content/project_components/Controllers/ControllerItems/ControllerField.js
+++ src/components/content/project_components/Controllers/ControllerItems/ControllerField.js
@@ -7,12 +7,12 @@ class ControllerField extends Component {
     }
 
     render() {
-        const { index, controllerid, whenData, doData, requireData } = this.props;
+        const { index, controllerid, whenData, doData, requireData, handleDeleteController, handleChangeInput } = this.props;
         return(
             <div className="contro-item">
             <div className="contro-item-inner">
                 <div className="project-item-header">
-                    <span className="delete-item" onClick={() => this.handleDeleteController(index)}> </span>
+                    <span className="delete-item" onClick={() => handleDeleteController(index)}> </span>
                 </div>
                 <div className="contro-item-title">
                     <input type="text" placeholder="" />
@@ -21,19 +21,19 @@ class ControllerField extends Component {
                     <div className="contro-row-container">
                         <label className="contro-row-name"> When </label>
                         <div className="contro-input-row">
-                            <input className="contro-input-field" type="text" id={controllerid} value={whenData} onChange={e => this.handleChangeInput(e, index, 'when_data')} onBlur={e => this.handleSaveChange(e, index)}/>
+                            <input className="contro-input-field" type="text" id={controllerid} value={whenData} onChange={e => handleChangeInput(e, index, 'when_data')} onBlur={e => this.handleSaveChange(e, index)}/>
                         </div>
                     </div>
                     <div className="contro-row-container">
                         <label className="contro-row-name"> Do </label>
                         <div className="contro-input-row">
-                            <input className="contro-input-field" type="text" id={controllerid} value={doData} onChange={e => this.handleChangeInput(e, index, 'do_data')} onBlur={e => this.handleSaveChange(e, index)}/>
+                            <input className="contro-input-field" type="text" id={controllerid} value={doData} onChange={e => handleChangeInput(e, index, 'do_data')} onBlur={e => this.handleSaveChange(e, index)}/>
                         </div>
                     </div>
                     <div className="contro-row-container">
                         <label className="contro-row-name"> Require </label>
                         <div className="contro-input-row">
-                            <input className="contro-input-field" type="text" id={controllerid} value={requireData} onChange={e => this.handleChangeInput(e, index, 'require_data')} onBlur={e => this.handleSaveChange(e, index)}/>
+                            <input className="contro-input-field" type="text" id={controllerid} value={requireData} onChange={e => handleChangeInput(e, index, 'require_data')} onBlur={e => this.handleSaveChange(e, index)}/>
                         </div>
                     </div>
                 </div>
