8bdc1b8ca09331f54e89390fa29d63ede267e5c1
Austin Wright <awguitar86@gmail.com>
2017-12-28 19:39:35 -0700

    split the Endpoints.js component into EndpointsField.js, ReqField.js and ResField.js then imported it into Endpoints.js

--- src/components/content/project_components/Endpoints/Endpoints.js
+++ src/components/content/project_components/Endpoints/Endpoints.js
@@ -2,28 +2,13 @@ import React, { Component } from 'react';
 import Header from '../../../Header/Header';
 import ProjectSidebar from '../ProjectSetupSidebar/ProjectSidebar'
 import './endpoints.scss';
+import EndpointsField from './EndpointsItem/EndpointsField';
 
 class Endpoints extends Component {
   constructor(props){
     super(props);
       this.state ={
-        endpoints: {
-                arr: [{
-                        key: "1",
-                        label: "1",
-                        url: "www.twitter.com/landonwjohnson",
-                        httpVerb: 'GET',
-                        request: [{
-                            field1: 'test',
-                            field2: 'test me again'
-                        }],
-                        response: [{
-                            field1: 'test',
-                            field2: 'test me again'
-                        }]
-                      }]
-
-        }
+        endpoints: []
       }
       this.addEndpointItemHandler = this.addEndpointItemHandler.bind(this);
       this.removeEndpointItemHandler = this.removeEndpointItemHandler.bind(this);
@@ -37,7 +22,7 @@ class Endpoints extends Component {
 
   //endpoint ITEM add and remove methods
   addEndpointItemHandler(){
-    let EndpointList = this.state.endpoints.arr;
+    let EndpointList = this.state.endpoints;
     EndpointList.push({
       key: 2,
       label: 2,
@@ -48,7 +33,7 @@ class Endpoints extends Component {
   }
 
   removeEndpointItemHandler(){
-    let EndpointList = this.state.endpoints.arr;
+    let EndpointList = this.state.endpoints;
     EndpointList.splice(1, 1);
     this.setState({arr: EndpointList})
   }
@@ -75,78 +60,10 @@ class Endpoints extends Component {
 
   render() {
     const { userid, projectid } = this.props.match.params;
-    const displayEndpoints = this.state.endpoints.arr.map( endpoint => {
-      return(
-        <div className="endpoint-item">
-        <div className="project-item-header">
-            <span onClick={this.removeEndpointItemHandler}> </span>
-        </div>
-        
-        <div className="endpoint-section-con">
-          <div className="endpoint-label-con">
-            {endpoint.label + '.'}
-          </div>
-          <div className="ep-field-con">
-              <input className="url-input-field" type="text" placeholder={endpoint.url} />
-              <select className="http-verb-select">
-                <option>GET</option>
-                <option>POST</option>
-                <option>UPDATE</option>
-                <option>DELETE</option>
-              </select>
-          </div>
-        </div>
-        
-        <div className="endpoint-divider"> </div>
-
-
-
-        <div className="endpoint-section-con-bottom">
-          <div className="ep-field-con-bottom" />
-
-              <div className="ep-bottom-row">
-                <div className="req-res-label">
-                  REQ
-                </div>
-                  <div className="req-res-wrapper">
-                    <input className="req-input-field" type="text" placeholder="username" />
-                    <span>:</span>
-                    <input className="req-input-field" type="text" placeholder="" />
-                  </div>
-              </div>
-              
-              <div className="ep-bottom-add-row">
-              <div className="req-res-label">
-                  
-                </div>
-              <button className="req-res-add" onClick={this.addEndpointRequestHandler}> + Add </button>
-              </div>
-        </div>
-
-
-        <div className="endpoint-section-con-bottom">
-          <div className="ep-field-con-bottom" />
-
-              <div className="ep-bottom-row">
-                <div className="req-res-label">
-                  RES
-                </div>
-                  <div className="req-res-wrapper">
-                    <input className="req-input-field" type="text" placeholder="username" />
-                    <span>:</span>
-                    <input className="req-input-field" type="text" placeholder="" />
-                  </div>
-              </div>
-              <div className="ep-bottom-add-row">
-              <div className="req-res-label">
-                  
-                </div>
-              <button className="req-res-add" onClick={this.addEndpointResponseHandler}> + Add </button>
-              </div>
-        </div>
-      </div>
-      )
-    })
+    const endpoints = this.state.endpoints;
+    const displayEndpoints = endpoints.map( endpoint => {
+      return<EndpointsField  removeEndpointItemHandler={endpoints.removeEndpointItemHandler} addEndpointRequestHandler={endpoints.addEndpointRequestHandler} addEndpointResponseHandler={endpoints.addEndpointResponseHandler}/>
+    });
     return (
         <div className="main-fix">
           <ProjectSidebar userid={userid} projectid={projectid}/>
