858a0457d4a81d2bef3d7e27aae6f98a8375fa34
Landon <landonwjohnson@gmail.com>
2018-01-04 11:55:55 -0700

    created sonic avatar svg

--- src/components/VUserBody/InfoBody/AccountSettings/modals/ChangeAvatar/AvatarIconGallery/AvatarIconGallery.js
+++ src/components/VUserBody/InfoBody/AccountSettings/modals/ChangeAvatar/AvatarIconGallery/AvatarIconGallery.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import LinkAvatar from '../../../../../../../img/User_Customization/avatars/link_avatar.svg';
 import BanjoAvatar from '../../../../../../../img/User_Customization/avatars/banjokazooie_avatar.svg';
 import HaloAvatar from '../../../../../../../img/User_Customization/avatars/mastercheif_avatar.svg';
-import SonicAvatar from '../../../../../../../img/User_Customization/avatars/SonicAvatar.JPG';
+import SonicAvatar from '../../../../../../../img/User_Customization/avatars/sonic_avatar.svg';
 import AvatarItem from './AvatarItem/AvatarItem';
 
 class AvatarIconGallery extends Component {
