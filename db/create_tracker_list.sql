795c5302a1cd62fe064b746f5e9a5d9179ce9228
Chad <chad.zuniga10@gmail.com>
2018-01-20 09:16:47 -0700

    tracker routers ready for testing

--- db/create_tracker_list.sql
+++ db/create_tracker_list.sql
@@ -5,5 +5,5 @@ INSERT INTO tracker_list (project_id, list_name, card_order_id, list_order)
 
 SELECT * FROM tracker_list
     WHERE project_id = $1
-    ORDER BY card_order_id
+    ORDER BY list_order
     ;
