5cab2fcdd69647a4f266ff91f2dff620a5ddaec8
Austin Wright <awguitar86@gmail.com>
2018-02-03 10:51:10 -0700

    added comma to update_project_endpoint.sql

--- db/update_project_endpoint.sql
+++ db/update_project_endpoint.sql
@@ -1,7 +1,7 @@
 UPDATE project_endpoint
     SET 
         endpoint_name = $3,
-        http_verb = $4
+        http_verb = $4,
         url_data = $5,
         response_data = $6,
         request_data = $7
